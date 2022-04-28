9edbae30acca91738e28945a45e13eb2ff91d289
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-04-28 09:21:45 -0300

    Criado UseCase para armazenar  os dados das images e id do carro

--- src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
@@ -1,3 +1,23 @@
-class UploadCarImageUseCase {
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { inject, injectable } from "tsyringe";
 
+interface IRequest {
+  car_id: string;
+  images_name: string[];
 }
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject("CarsImagesRepository")
+    private carsImagesRepository: ICarsImagesRepository
+  ) { }
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async (image_name) => {
+      await this.carsImagesRepository.create(car_id, image_name)
+    })
+  }
+}
+
+export { UploadCarImageUseCase }
